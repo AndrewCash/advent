3e1d536124000cf36d4086ae97ea597cd3ce4177
Andrew Cash <cashandrew41@gmail.com>
2020-12-01 12:27:51 -0600

    2020 Day 1

---
+++ 2020/day1/day1.py
@@ -0,0 +1,29 @@
+f = open('input.txt', 'r+')
+input_data = 
+f.close
+
+print(input_data[2])
+
+# for i in input_data:
+#     print("i= " +str(i))
+    
+    # sum = 0
+
+    # for j in input_data:
+    #     #print(" j = " +str(j))
+
+    #     for k in input_data:
+
+    #         sum = i + j + k
+    #         #print(sum)
+    #         if sum == 2020:
+    #             print ("The sum of " + str(i) + " and " + str(j) + " and " + str(k) + " is " + str(sum))
+    #             multiplied = i * j * k
+    #             print ("Multipled they make " + str(multiplied))
+    #             break
+
+    # if sum==2020:
+    #     break
+    # else:
+    #     sum = 0
+
