ab103654f152de79c6c1ba26f62275609e4f31c5
Andrew Cash <andrew@csh.casa>
2023-12-08 00:19:52 -0600

    add tests for part 2

--- 2023/day1/test.py
+++ 2023/day1/test.py
@@ -1,5 +1,6 @@
 import unittest
 import part1
+import part2
 
 
 class TestPartOne(unittest.TestCase):
@@ -18,6 +19,68 @@ class TestPartOne(unittest.TestCase):
     def test_find_sum(self):
         data = [12, 38, 15, 77]
         self.assertEqual(part1.find_sum(data), 142)
+
+
+class TestPartTwo(unittest.TestCase):
+    def test_correct_calibration_case_one(self):
+        value = part2.correct_calibration_value("two1nine")
+        self.assertEqual(value, 219)
+
+    def test_parse_calibration_case_one(self):
+        value = part2.parse_calibration_value("219")
+        self.assertEqual(value, 29)
+
+    def test_correct_calibration_case_two(self):
+        value = part2.correct_calibration_value("eightwothree")
+        self.assertEqual(value, 823)
+
+    def test_parse_calibration_case_two(self):
+        value = part2.parse_calibration_value("823")
+        self.assertEqual(value, 83)
+
+    def test_correct_calibration_case_three(self):
+        value = part2.correct_calibration_value("abcone2threexyz")
+        self.assertEqual(value, "abc123xyz")
+
+    def test_parse_calibration_case_three(self):
+        value = part2.parse_calibration_value("abc123xyz")
+        self.assertEqual(value, 13)
+
+    def test_correct_calibration_case_four(self):
+        value = part2.correct_calibration_value("xtwone3four")
+        self.assertEqual(value, "x2ne34")
+
+    def test_parse_calibration_case_four(self):
+        value = part2.parse_calibration_value("x2ne34")
+        self.assertEqual(value, 24)
+
+    def test_correct_calibration_case_five(self):
+        value = part2.correct_calibration_value("4nineeightseven2")
+        self.assertEqual(value, "49872")
+
+    def test_parse_calibration_case_five(self):
+        value = part2.parse_calibration_value("49872")
+        self.assertEqual(value, 42)
+
+    def test_correct_calibration_case_six(self):
+        value = part2.correct_calibration_value("zoneight234")
+        self.assertEqual(value, "z1ight234")
+
+    def test_parse_calibration_case_six(self):
+        value = part2.parse_calibration_value("z1ight234")
+        self.assertEqual(value, 14)
+
+    def test_correct_calibration_case_seven(self):
+        value = part2.correct_calibration_value("7pqrstsixteen")
+        self.assertEqual(value, "7pqrst16")
+
+    def test_parse_calibration_case_seven(self):
+        value = part2.parse_calibration_value("7pqrst16")
+        self.assertEqual(value, 76)
+
+    def test_find_sum(self):
+        data = [29, 83, 13, 24, 42, 14, 76]
+        self.assertEqual(part2.find_sum(data), 281)
 
 
 if __name__ == "__main__":
