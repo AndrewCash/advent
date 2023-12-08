b749b502495914e392256c2809a069a375f9d45f
Andrew Cash <andrew@csh.casa>
2023-12-08 00:02:27 -0600

    clean up tests

--- 2023/day1/test.py
+++ 2023/day1/test.py
@@ -3,31 +3,21 @@ import part1
 
 
 class TestPartOne(unittest.TestCase):
-    def setUp(self):
-        self.data = []
-
     def test_case_one(self):
-        value = part1.parse_calibration_value("1abc2")
-        self.data.append(value)
-        self.assertEqual(value, 12)
+        self.assertEqual(part1.parse_calibration_value("1abc2"), 12)
 
     def test_case_two(self):
-        value = part1.parse_calibration_value("pqr3stu8vwx")
-        self.data.append(value)
-        self.assertEqual(value, 38)
+        self.assertEqual(part1.parse_calibration_value("pqr3stu8vwx"), 38)
 
     def test_case_three(self):
-        value = part1.parse_calibration_value("a1b2c3d4e5f")
-        self.data.append(value)
-        self.assertEqual(value, 15)
+        self.assertEqual(part1.parse_calibration_value("a1b2c3d4e5f"), 15)
 
     def test_case_four(self):
-        value = part1.parse_calibration_value("treb7uchet")
-        self.data.append(value)
-        self.assertEqual(value, 77)
+        self.assertEqual(part1.parse_calibration_value("treb7uchet"), 77)
 
-    def test_final_answer(self):
-        self.assertEqual(part1.find_sum(self.data), 142)
+    def test_find_sum(self):
+        data = [12, 38, 15, 77]
+        self.assertEqual(part1.find_sum(data), 142)
 
 
 if __name__ == "__main__":
